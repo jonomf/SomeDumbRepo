5aab2c5ef73cc25baa936a3df41c55b44c52c948
Jono <jono@defectivestudios.com>
2015-08-04 19:41:40 -0400

    Wrote some stupid log.

--- Assets/Scripts/SomeAwesomeScript.cs
+++ Assets/Scripts/SomeAwesomeScript.cs
@@ -5,7 +5,7 @@ public class SomeAwesomeScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		Debug.Log("what a silly script");
 	}
 	
 	// Update is called once per frame
